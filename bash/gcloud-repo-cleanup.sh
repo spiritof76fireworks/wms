9104e2ff35cb2415d827b30924f7bf1f9b79ce90
Kyle Hurt <kyle@76fireworks.com>
2020-07-09 14:15:00 -0500

    wms-1089

--- bash/gcloud-repo-cleanup.sh
+++ bash/gcloud-repo-cleanup.sh
@@ -3,4 +3,8 @@
 
 . ./gcloud-init-auth.sh
 
+echo "OLD_IMAGES_TO_KEEP"
+echo $OLD_IMAGES_TO_KEEP
+
+images_to_keep=5
 . ./repo-cleanup-loop.sh
--- bash/repo-cleanup-loop.sh
+++ bash/repo-cleanup-loop.sh
@@ -3,19 +3,6 @@ latest=`kubectl get deployment $APP_NAME-server-deployment -o jsonpath="{..image
 count=1
 image_path=$REPOSITORY/$PROJECT_NAME/$APP_NAME
 
-echo $image_path
-images_to_keep = 10
-
-echo $images_to_keep
-
-if [ -z "$OLD_IMAGES_TO_KEEP" ]; then
-    echo "OLD_IMAGES_TO_KEEP"
-    echo $OLD_IMAGES_TO_KEEP
-    images_to_keep=$OLD_IMAGES_TO_KEEP
-    echo "in if"
-    echo $images_to_keep
-fi
-
 for digest in $(gcloud container images list-tags $image_path --limit=9999 --sort-by=~TIMESTAMP --filter="NOT tags = $latest" --format='get(digest)' );
 do
     if [ $count -gt $images_to_keep ]; then
