cbd06f0cf66da45fb304e02a44c9526a3fd59478
Kyle Hurt <kyle@76fireworks.com>
2020-07-09 14:20:08 -0500

    wms-1089

--- bash/gcloud-repo-cleanup.sh
+++ bash/gcloud-repo-cleanup.sh
@@ -4,13 +4,13 @@
 . ./gcloud-init-auth.sh
 
 if [ -z "$OLD_IMAGES_TO_KEEP" ]; then
+    images_to_keep=10
+else
     echo "OLD_IMAGES_TO_KEEP"
     echo $OLD_IMAGES_TO_KEEP
     images_to_keep=$OLD_IMAGES_TO_KEEP
     echo "in if"
     echo $images_to_keep
-else
-    images_to_keep=10
 fi
 
 . ./repo-cleanup-loop.sh
