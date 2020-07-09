4129df3f57c53fc4ff8181eecac0210119ed8a3f
Kyle Hurt <kyle@76fireworks.com>
2020-07-09 13:24:33 -0500

    wms-1089

--- bash/repo-cleanup-loop.sh
+++ bash/repo-cleanup-loop.sh
@@ -1,7 +1,9 @@
 latest=`kubectl get deployment $APP_NAME-server-deployment -o jsonpath="{..image}" |awk -F ":" '{print $2}'`
 
 count=1
-image_path = $REPOSITORY/$PROJECT_NAME/$APP_NAME
+image_path=$REPOSITORY/$PROJECT_NAME/$APP_NAME
+
+echo $image_path
 
 for digest in $(gcloud container images list-tags $image_path --limit=9999 --sort-by=~TIMESTAMP --filter="NOT tags = $latest" --format='get(digest)' );
 do
