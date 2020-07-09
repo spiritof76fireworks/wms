5651cb4c3669179d9019c9deb4ee149913666cf2
Kyle Hurt <kyle@76fireworks.com>
2020-07-09 14:10:27 -0500

    wms-1089

--- bash/repo-cleanup-loop.sh
+++ bash/repo-cleanup-loop.sh
@@ -6,8 +6,14 @@ image_path=$REPOSITORY/$PROJECT_NAME/$APP_NAME
 echo $image_path
 images_to_keep = 10
 
-if [ -z "$var" ]; then
+echo $images_to_keep
+
+if [ -z "$OLD_IMAGES_TO_KEEP" ]; then
+    echo "OLD_IMAGES_TO_KEEP"
+    echo $OLD_IMAGES_TO_KEEP
     images_to_keep=$OLD_IMAGES_TO_KEEP
+    echo "in if"
+    echo $images_to_keep
 fi
 
 for digest in $(gcloud container images list-tags $image_path --limit=9999 --sort-by=~TIMESTAMP --filter="NOT tags = $latest" --format='get(digest)' );
